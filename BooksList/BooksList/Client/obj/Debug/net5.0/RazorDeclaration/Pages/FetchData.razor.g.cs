1257822a4b70a883a3399df646c639f1aaeaf923
mariru27 <marina.rusu.99@gmail.com>
2021-07-06 18:14:24 +0300

    display an card

--- BooksList/BooksList/Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ BooksList/BooksList/Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -90,6 +90,20 @@ using BooksList.Shared.Models;
 #line hidden
 #nullable disable
 #nullable restore
+#line 12 "C:\Users\Marina Rusu\Desktop\BooksList\BooksList\BooksList\Client\_Imports.razor"
+using BooksList.Client.IServices;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\Marina Rusu\Desktop\BooksList\BooksList\BooksList\Client\_Imports.razor"
+using BooksList.Client.Services;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
 #line 2 "C:\Users\Marina Rusu\Desktop\BooksList\BooksList\BooksList\Client\Pages\FetchData.razor"
 using BooksList.Shared;
 
--- BooksList/BooksList/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ BooksList/BooksList/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -89,6 +89,20 @@ using BooksList.Shared.Models;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 12 "C:\Users\Marina Rusu\Desktop\BooksList\BooksList\BooksList\Client\_Imports.razor"
+using BooksList.Client.IServices;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\Marina Rusu\Desktop\BooksList\BooksList\BooksList\Client\_Imports.razor"
+using BooksList.Client.Services;
+
+#line default
+#line hidden
+#nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/")]
     public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
     {
