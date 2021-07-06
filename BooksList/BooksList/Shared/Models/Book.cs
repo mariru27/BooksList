d85c0990b77d6ce8973c84cbf910e9e14f33bb09
mariru27 <marina.rusu.99@gmail.com>
2021-07-06 11:29:44 +0300

    Book model

--- BooksList/BooksList/Shared/Models/Book.cs
+++ BooksList/BooksList/Shared/Models/Book.cs
@@ -1,7 +1,13 @@
-﻿namespace BooksList.Shared.Models
-{
-    class Book
-    {
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace BooksList.Shared.Models
+{
+    public class Book
+    {
+        [Key]
+        public int IdBook { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public State State { get; set; }
     }
 }
