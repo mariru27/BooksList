136f2ee794b5b18f7ded7d9b7036775aaa050a61
mariru27 <marina.rusu.99@gmail.com>
2021-07-08 16:58:30 +0300

    implement Get states

---
+++ BooksList/BooksList/Server/Controllers/StatesController.cs
@@ -0,0 +1,56 @@
+﻿using BooksList.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BooksList.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : ControllerBase
+    {
+        BooksListContext _context;
+        public StatesController(BooksListContext booksListContext)
+        {
+            _context = booksListContext;
+        }
+
+        // GET: api/<StatusController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StatusController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var states = await _context.States.Where(a => a.IdState == id).ToListAsync();
+            return Ok(states);
+        }
+
+        // POST api/<StatusController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StatusController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StatusController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
