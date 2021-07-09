9686c96252b4f2cedb73aa16f844d73eba7d2bb1
DESKTOP-503D7S8\Marina Rusu <marina.rusu.99@gmail.com>
2021-07-09 15:10:59 +0300

    add BookViewModel

---
+++ BooksList/BooksList/Client/ViewModel/BookViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BooksList.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksList.Client.ViewModel
+{
+    public class BookViewModel
+    {
+        [Key]
+        public int IdBook { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Author { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public int StateId { get; set; }
+        public State State { get; set; }
+    }
+}
